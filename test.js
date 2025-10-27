ae79fd6de76b618f518db3191ff6919d2f932f35
chibuzor <ezehnwabueze6@gmail.com>
2025-10-27 15:06:22 +0100

    added test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+// write a function that takes an array of strings and converts their length property to numbers, returning the same array ;
+
+const isPalindrome = (str) => { 
+  return str.split(``).join(``) === str.split(``).reverse().join(``);
+} ;
+
+console.log (isPalindrome(`racecar`)) ;
+console.log (`bean`.split(``).reverse()) ;
+
+const numberRep = (arr) => {
+  return arr.map((index) => {
+    return index.charCodeAt();
+  });
+};
+
+console.log(numberRep([`r`, `a`])); 
+
+console.log () ;
